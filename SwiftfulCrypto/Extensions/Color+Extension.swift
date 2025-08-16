1c59d3ffe6722642e2d50c285559244b47cc27ea
Игорь Чумиков <igorcumikov@MacBook-Pro-Igor.local>
2025-08-16 14:47:26 +0700

    add Color+Extension

---
+++ SwiftfulCrypto/Extensions/Color+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Extension.swift
+//  SwiftfulCrypto
+//
+//  Created by Игорь Чумиков on 16.08.2025.
+//
+
+import SwiftUI
+
+extension Color {
+    static let there = ColorThere()
+}
+
+struct ColorThere {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
