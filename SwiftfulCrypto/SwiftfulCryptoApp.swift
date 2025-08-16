34abd04c6b9e577ac02c86af7b9a0e0feb7e0f21
Игорь Чумиков <igorcumikov@MacBook-Pro-Igor.local>
2025-08-16 14:28:42 +0700

    add

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Игорь Чумиков on 16.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
